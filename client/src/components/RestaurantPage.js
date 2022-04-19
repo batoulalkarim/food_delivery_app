563192d68f0a36295d91339a592f4dc61a22e93f
William Warner Burke <williamburke121@gmail.com>
2022-04-19 17:00:43 -0400

    added logout

--- client/src/components/RestaurantPage.js
+++ client/src/components/RestaurantPage.js
@@ -3,20 +3,20 @@ import React, {useState, useEffect} from "react"
 const RestaurantPage = ({resPage, view}) => {
     const [restrs, setRestrs] = useState([])
 
-    useEffect(() => {
-        fetch('http://localhost:3000/restaurants')
-        .then(res => res.json())
-        .then(data => {
-            setRestrs(data)
-        }) 
-    }, [])
+    // useEffect(() => {
+    //     fetch('http://localhost:3000/restaurants')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         setRestrs(data)
+    //     }) 
+    // }, [])
     
     return(
         <div>
             {restrs.map(r => {
                 return(
                     <div>
-                        <h1>{r.name}</h1>
+                        <h1>{''}</h1>
                     </div>
                 )
             })}
