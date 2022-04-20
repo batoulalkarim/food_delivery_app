12e9b815120edcbeaefe367b93f6d7a66bf4df9b
William Warner Burke <williamburke121@gmail.com>
2022-04-20 15:05:06 -0400

    nested resources and assigned backend data to each restaurant final

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+    def index
+        return render json: Item.where(restaurant_id:[params[:restaurant_id]]), status: 200
+    end
+    def show 
+        item = Item.find_by(id: params[:id])
+        if item
+            return render json: item
+        else
+            return render json:{ error: "Not Found"}, status: :not_found
+        end 
+    end 
 end
